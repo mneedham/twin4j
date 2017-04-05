464cd881535b894ab003393caa1700a109d2a3cd
Mark Needham <mark.needham@neotechnology.com>
2017-04-05 17:12:51 +0100

    script to generate HTML

---
+++ gdoc.sh
@@ -0,0 +1,6 @@
+echo 'include::{url}[]' > template.adoc
+asciidoctor -a allow-uri-read \
+  -a url='https://docs.google.com/a/neotechnology.com/document/export?format=txt&id=1fWi-8IIkeLHvp9FxGlg6EODKu6Gs4GKIv65t3HybooI' \
+  template.adoc \
+  -T templates \
+  -o -
