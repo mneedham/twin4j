04bc928edd4e6f97737250a5a902b288619a0920
Mark Needham <mark.needham@neotechnology.com>
2017-06-21 16:15:26 +0100

    printscreen that tweet

--- lib/twitter-email-macro/extension.rb
+++ lib/twitter-email-macro/extension.rb
@@ -1,12 +1,15 @@
 require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
 
+require 'webdriver-user-agent'
+require 'selenium-webdriver'
+
 include ::Asciidoctor
 
-# A block macro that embeds a Gist into the output document
+# A block macro that embeds a Tweet into a document
 #
 # Usage
 #
-#   gist::12345[]
+#   tweet::12345[]
 #
 class TwitterBlockMacro < Extensions::BlockMacroProcessor
   use_dsl
@@ -14,14 +17,27 @@ class TwitterBlockMacro < Extensions::BlockMacroProcessor
   named :tweet
 
   def process parent, target, attrs
-    puts target
-    title_html = (attrs.has_key? 'title') ?
-        %(<div class="title">#{attrs['title']}</div>\n) : nil
 
-    html = %(<div class="openblock gist">
-#{title_html}<div class="content">
-<script src="https://gist.github.com/#{target}.js"></script>
-</div>
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-popup-blocking')
+    options.add_argument('--disable-translate')
+    options.add_argument('--user-agent=iphone')
+    options.add_argument('--window-size=414,600')
+
+    image_location = "adoc/images/#{target}.png"
+    tweet_location = "https://twitter.com/twin4j/status/#{target}"
+
+    driver = Selenium::WebDriver.for :chrome, options: options
+    driver.navigate.to "https://twitter.com/twin4j/status/#{target}"
+    driver.save_screenshot image_location
+
+    driver.quit
+
+    html = %(<div class="content">
+    <a class="image" href="#{tweet_location}" target="_blank">
+      <img src="#{image_location}" width="100%" />
+    </a>
 </div>)
 
     create_pass_block parent, html, attrs, subs: nil
