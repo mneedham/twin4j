10239b18face53f03f68e04062960e2d353764b8
Mark Needham <mark.needham@neotechnology.com>
2017-06-21 16:42:50 +0100

    now creates tweet screenshot or inlines tweet

--- lib/twitter-email-macro/extension.rb
+++ lib/twitter-email-macro/extension.rb
@@ -2,6 +2,7 @@ require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
 
 require 'webdriver-user-agent'
 require 'selenium-webdriver'
+require 'open-uri'
 
 include ::Asciidoctor
 
@@ -18,28 +19,38 @@ class TwitterBlockMacro < Extensions::BlockMacroProcessor
 
   def process parent, target, attrs
 
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--disable-popup-blocking')
-    options.add_argument('--disable-translate')
-    options.add_argument('--user-agent=iphone')
-    options.add_argument('--window-size=414,600')
+    if attrs["type"] == "web"
+      response = open('https://publish.twitter.com/oembed?url=https://twitter.com/twin4j/status/877476151912005632').read
+      inner_html = JSON.parse(response)["html"]
 
-    image_location = "adoc/images/#{target}.png"
-    tweet_location = "https://twitter.com/twin4j/status/#{target}"
+      html = %(<div class="content">
+      #{inner_html}
+      </div>)
+    else
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--ignore-certificate-errors')
+      options.add_argument('--disable-popup-blocking')
+      options.add_argument('--disable-translate')
+      options.add_argument('--user-agent=iphone')
+      options.add_argument('--window-size=414,600')
 
-    driver = Selenium::WebDriver.for :chrome, options: options
-    driver.navigate.to "https://twitter.com/twin4j/status/#{target}"
-    driver.save_screenshot image_location
+      image_location = "adoc/images/#{target}.png"
+      tweet_location = "https://twitter.com/twin4j/status/#{target}"
 
-    driver.quit
+      driver = Selenium::WebDriver.for :chrome, options: options
+      driver.navigate.to "https://twitter.com/twin4j/status/#{target}"
+      driver.save_screenshot image_location
 
-    html = %(<div class="content">
-    <a class="image" href="#{tweet_location}" target="_blank">
-      <img src="#{image_location}" width="100%" />
-    </a>
-</div>)
+      driver.quit
+
+      html = %(<div class="content">
+      <a class="image" href="#{tweet_location}" target="_blank">
+        <img src="#{image_location}" width="100%" />
+      </a>
+      </div>)
+    end
 
     create_pass_block parent, html, attrs, subs: nil
+
   end
 end
--- newsletter.sh
+++ newsletter.sh
@@ -1,5 +1,7 @@
 asciidoctor -a allow-uri-read \
   -a url='adoc/2017-06-17.adoc' \
+  -a type=newsletter \
+  -r ./lib/twitter-email-macro.rb \
   template.adoc \
   -T newsletter-templates \
   -o -
