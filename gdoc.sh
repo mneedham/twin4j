9b98334c990e029e4ac21832c692ae76031adf45
Mark Needham <mark.needham@neotechnology.com>
2018-07-20 15:03:30 +0100

    adding template

--- gdoc.sh
+++ gdoc.sh
@@ -4,7 +4,7 @@
 
 # : ${doc_id:=${1:-"1CCM7BZjEVs_wLkm35X7o43PW0ILbZIGKbamOVmklxRQ"}}
 
-: ${doc_id:=${1:-"1rR1esccQGMbL7pPwzfgD74JdRL3SMO-LoviFEBtiJOY"}}
+: ${doc_id:=${1:-"114W3o8Crki3CjeIus79ZMMsJk4_SuMEIfvSL6YRLUe8"}}
 
 
 asciidoctor -a allow-uri-read \
@@ -20,4 +20,4 @@ asciidoctor -a allow-uri-read \
 # https://docs.google.com/a/neotechnology.com/document/export?format=txt&id=1CCM7BZjEVs_wLkm35X7o43PW0ILbZIGKbamOVmklxRQ&token=AC4w5VhAIPJGICN_cpnQeRq2A63capF8Pg%3A1516032508978&includes_info_params=true
 # https://docs.google.com/a/neotechnology.com/document/export?format=txt&id=1CCM7BZjEVs_wLkm35X7o43PW0ILbZIGKbamOVmklxRQ&token=AC4w5VhAIPJGICN_cpnQeRq2A63capF8Pg%3A1516032508978&includes_info_params=true
 
-for file in `find adoc -name \*.adoc`; do echo $file; asciidoctor $file -a type=web -r ./lib/twitter-macro.rb -r ./lib/youtube-macro.rb -r ./lib/featured-macro.rb; done 
+# for file in `find adoc -name \*.adoc`; do echo $file; asciidoctor $file -a type=web -r ./lib/twitter-macro.rb -r ./lib/youtube-macro.rb -r ./lib/featured-macro.rb; done
