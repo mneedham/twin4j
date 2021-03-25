fc233b308dd6c9e383b8b784c77fa4a5e92d9748
Michael Hunger <github@jexp.de>
2021-03-25 22:56:35 +0100

    Updated gdoc.sh

--- gdoc.sh
+++ gdoc.sh
@@ -2,20 +2,19 @@
 
 # echo 'include::{url}[]' > template.adoc
 
-# : ${doc_id:=${1:-"1CCM7BZjEVs_wLkm35X7o43PW0ILbZIGKbamOVmklxRQ"}}
-
-
 : ${doc_id:=${1:-"1npXygcsWG0Vnvop50_rB1BebiACHxfPV77h6A8pZ-hU"}}
+echo -e ${doc_id}
 
-
-asciidoctor -a allow-uri-read \
-  -a url="https://docs.google.com/a/neotechnology.com/document/export?format=txt&id=${doc_id}" \
+rbenv exec asciidoctor -a allow-uri-read \
+  -a url="https://docs.google.com/a/neotechnology.com/document/export?format=txt&id=$doc_id" \
   template.adoc \
+  -a allow-uri-read \
   -a type=web \
   -r ./lib/twitter-macro.rb \
   -r ./lib/youtube-macro.rb \
   -r ./lib/featured-macro.rb  \
   -T blog-templates \
+  --trace \
   -o -
 
 # https://docs.google.com/a/neotechnology.com/document/export?format=txt&id=1CCM7BZjEVs_wLkm35X7o43PW0ILbZIGKbamOVmklxRQ&token=AC4w5VhAIPJGICN_cpnQeRq2A63capF8Pg%3A1516032508978&includes_info_params=true
